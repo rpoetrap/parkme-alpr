32dd6788c02907ec4eda088fb790409794e91a4d
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-06-30 15:29:05 +0700

    Add darknet config

---
+++ src/configs/darknet.ts
@@ -0,0 +1,12 @@
+import { Darknet } from 'darknet';
+import path from 'path';
+
+// Load darknet model
+const darknetConfig = './model.cfg';
+const darknetWeights = './model.weights';
+
+export const darknet = new Darknet({
+  weights: path.join(__dirname, darknetWeights),
+  config: path.join(__dirname, darknetConfig),
+  names: ['plate']
+});
