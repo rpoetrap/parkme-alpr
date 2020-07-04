93b5a9a727306959e98178600f60a26356b0d94f
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-07-04 14:58:32 +0700

    Add backpro

---
+++ src/helpers/Backpropagation.ts
@@ -0,0 +1,91 @@
+import { last, head, findLastIndex } from 'lodash';
+import { table, getBorderCharacters, TableUserConfig } from 'table';
+import {
+  matrix,
+  random,
+  subtract,
+  add,
+  Matrix,
+  multiply,
+  exp,
+  map,
+  transpose
+} from 'mathjs';
+
+const tableConfig: TableUserConfig = {
+  border: getBorderCharacters('norc')
+}
+
+export default class Backpropagation {
+  private outputs: string[];
+  private weights: Matrix[] = [];
+  private bias: Matrix[] = [];
+
+  constructor(inputNode: number, hiddenNodes: number[], outputs: string[]) {
+    this.outputs = outputs;
+    for (const [idx, nodes] of hiddenNodes.entries()) {
+      const previousNode = idx == 0 ? inputNode : hiddenNodes[idx - 1];
+      const hiddenWeights = subtract(matrix(random([nodes, previousNode], 0, 2)), 1) as Matrix;
+      this.weights.push(hiddenWeights);
+
+      const hiddenBias = matrix(random([nodes, 1]));
+      this.bias.push(hiddenBias);
+    }
+
+    const outputWeights = subtract(matrix(random([this.outputs.length, last(hiddenNodes)!], 0, 2)), 1) as Matrix;
+    this.weights.push(outputWeights);
+
+    const outputBias = matrix(random([this.outputs.length, 1]));
+    this.bias.push(outputBias);
+  }
+
+  activation(x: number, derivative = false) {
+    if (derivative) return x * (1 - x);
+    return 1 / (1 + exp(-x));
+  }
+
+  train(inputArray: number[], targetArray: string) {
+    const input = matrix(inputArray).resize([inputArray.length, 1]);
+    const hiddens: Matrix[] = [];
+    let output = matrix();
+
+    for (const [idx, weight] of this.weights.entries()) {
+      const previousLayer = idx == 0 ? input : output;
+      output = multiply(weight, previousLayer);
+      output = add(output.toArray(), this.bias[idx]) as Matrix;
+      // Activation
+      output = map(output, (x) => this.activation(x));
+
+      if (idx != findLastIndex(this.weights)) hiddens.push(output)
+    }
+
+    const targets = matrix(this.outputs.map(output => output === targetArray ? [1] : [0]));
+    const outputErrors = subtract(targets, output) as Matrix;
+    let hiddenErrors: Matrix[] = [];
+
+    for (let i = findLastIndex(this.weights); i > 0; i--) {
+      const previousError = i == findLastIndex(this.weights) ? outputErrors : head(hiddenErrors)!;
+      const transposedWeight = transpose(this.weights[i]);
+
+      const error = multiply(transposedWeight, previousError);
+      hiddenErrors = [error, ...hiddenErrors];
+    }
+
+    // PRINT TABLE
+    console.log(table([['INPUT LAYER'], ...input.toArray() as any], tableConfig));
+    for (const [id, hidden] of hiddens.entries()) {
+      console.log(table([
+        ['HIDDEN LAYER', 'ERROR'],
+        ...(hidden.toArray() as any[]).map((item, idx) => {
+          return [...item, ...(hiddenErrors[id].toArray() as any[])[idx]];
+        })
+      ], tableConfig));
+    }
+    console.log(table([
+      ['OUTPUT LAYER', 'ERROR'],
+      ...(output.toArray() as any[]).map((item, idx) => {
+        return [...item, ...(outputErrors.toArray() as any[])[idx]];
+      })
+    ], tableConfig));
+  }
+}
