06b0076fd0658b0c54e8eb11a4826f20cf1145f6
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-07-18 06:28:08 +0700

    Add config

---
+++ src/migrations/20200717083900_create_configs_table.ts
@@ -0,0 +1,18 @@
+import * as Knex from 'knex';
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('configs', table => {
+    table.increments('id');
+    table.string('key').unique();
+    table.string('value');
+    table.dateTime('created_at').notNullable().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').notNullable().defaultTo(knex.fn.now());
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('configs');
+}
+
---
+++ src/models/Config.ts
@@ -0,0 +1,15 @@
+import { Model } from 'objection';
+
+class Config extends Model {
+  id: number;
+  key: string;
+  value: string;
+  created_at: string;
+  updated_at: string;
+
+  static get tableName() {
+    return 'configs';
+  }
+}
+
+export default Config;
