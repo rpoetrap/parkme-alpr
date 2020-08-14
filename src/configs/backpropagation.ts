50544602be74607a98373dfca2cb7548e0b41416
rpoetrap <rzkputrapamungkas65@gmail.com>
2020-08-15 06:39:07 +0700

    Update learning rate and error

--- src/configs/backpropagation.ts
+++ src/configs/backpropagation.ts
@@ -1,8 +1,8 @@
 import path from 'path';
 import fs from 'fs';
 
-export const toleransiError = 0.0005;
-export const learningRate = 0.001;
+export const toleransiError = 0.00045;
+export const learningRate = 0.5;
 export const hiddenLayer = [50, 50];
 
 // Load list of characters
